dbfaeb8c94341f8c5950888b1692a1c81f0cdd6b
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-01-23 23:43:11 -0300

    :wreWrench/add middlewares (#5)
    * :zap: Add CORS and cache middlewares
    
    * :zap: Update README.md

--- main.go
+++ main.go
@@ -2,16 +2,37 @@ package main
 
 import (
 	"log"
+	"os"
+	"time"
 
 	"github.com/esequiel378/unsplashed-bff/services"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	app := fiber.New()
+
+	if origins := os.Getenv("ALLOW_ORIGINS"); origins != "" {
+		app.Use(cors.New(cors.Config{
+			AllowOrigins: origins,
+		}))
+	}
+
+	app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("refresh") == "true"
+		},
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+	}))
 
 	servicesRoutes := services.GetRoutes()
 
